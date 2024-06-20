c280ac27d165b9d35cfd2ef3cc051c7dff38ec4e
whitebow01 <whitebownitro@gmail.com>
2024-06-19 21:35:46 -0400

    UPDATE se actualizan nombre de variables registro trabajadores

--- registros.py
+++ registros.py
@@ -5,20 +5,20 @@ registroTrabajadores = [{1:"15546854-5", 2:"Juan", 3: "Castro",4:"sss"}]
 
 def registroTrabajador():
     registro = {1:"", 2:"",3:"", 4:""}
-    rut = input("Ingrese su rut\n")
-    registro[1] = rut
-    nombre = input("Ingrese su nombre \n")
-    registro[2] = nombre
-    apellido = input("Ingrese su apellido \n")
-    registro[3] = apellido
-    contraseña = input("Ingrese contraseña \n")
-    registro[4] = contraseña
+    rutTrabajador = input("Ingrese su rut\n")
+    registro[1] = rutTrabajador
+    nombreTrabajador = input("Ingrese su nombre \n")
+    registro[2] = nombreTrabajador
+    apellidoTrabajador = input("Ingrese su apellido \n")
+    registro[3] = apellidoTrabajador
+    contraseñaTrabajador = input("Ingrese contraseña \n")
+    registro[4] = contraseñaTrabajador
 
     registroTrabajadores.append(registro)
 
-    print(f"Bienvenido, {nombre}, ahora puede iniciar sesion")
+    print(f"Bienvenido, {nombreTrabajador}, ahora puede iniciar sesion")
 
-    print(f"Tus datos:  \n rut: {rut} \n nombre: {nombre}\n apellido:{apellido}\n contraseña: """)
+    print(f"Tus datos:  \n rut: {rutTrabajador} \n nombre: {nombreTrabajador}\n apellido:{apellidoTrabajador}\n contraseña: """)
 
 
     ##MENU ADMIN - REGISTRO CLIENTE
