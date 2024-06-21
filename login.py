535dc42a210d7c82fab0efe1c4985ddd9aca58fc
whitebow01 <whitebownitro@gmail.com>
2024-06-21 17:07:33 -0400

    CLEAN se eliminan codigos comentados y se cambia el menu por un llamado al menu con fucion

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
 import os,time
 import registros
-
+import crudSistemaReserva
 def loginTrabajador():
     usuarioCorrecto = False
     contraseñaCorrecta = False
@@ -22,50 +22,16 @@ def loginTrabajador():
         if passwordTrabajador == usuario[4]:
             contraseñaCorrecta = True
             print("Contraseña correcta")
-            print(f"Bienvenido, {registros.registroTrabajadores[2]}")
+            print(f"Bienvenido, ")
         else:
             print("Contraseña incorrecta")
-            
-            
-            
-    # while not usuarioCorrecto or not contraseñaCorrecta:
-    #     login_name = input("Ingrese su rut \n")
-    #     listaTrabajadores = registros.registroTrabajadores#Esto llama a al registro con los trabajadores
-    #     #for usuario in registros.registroTrabajador: #Esto llama a la funcion no al registro db
-    #     for usuario in listaTrabajadores: 
+    #MENU USUARIO LOGEADO       
+    if usuarioCorrecto and contraseñaCorrecta == True:
+        respuesta = "si" 
 
-    #         if login_name == usuario[1]:
-    #             usuarioCorrecto = True
-    #             print("Nombre de usuario correcto")
-    #             login_password = input("Ingresa tu contraseña \n")
-    #             if login_password == usuario[4]:
-    #                 contraseñaCorrecta = True
-    #                 print("Contraseña correcta")
-    #                 print(f"Bienvenido, usuario {login_name}")
-    #             else:
-    #                 print("Contraseña incorrecta")
-    #                 usuarioCorrecto = False  # Reiniciar si la contraseña es incorrecta
-    #     if not usuarioCorrecto:
-    #         print("Nombre de usuario incorrecto")
-        
-        
-        
-        
-        # userBan=False
-        # acceso=False
-        # user=int(input("Ingrese su rut sin número verificador ni guíon \n")) 
-        # password=input("Ingrese su contraseña: \n")
-        # for x in registros.registroClientes:
-        #     clienteBan=user in x 
-        #     aux=x             #busca si clave existe en la lista
-        #     if(clienteBan):
-        #         print("Nombre correcto")
-        #         break
-        # if(clienteBan):
-        #     clave=x[user]
-        #     if(clave==password):
-        #         print(f"Bienvenido, {registros.nombre}, clave correcta")
-        #     acceso=True
-        #     break
-        # else:
-        #     print("El usuario o la clave son incorrecto")  
+        crudSistemaReserva.menuAdmin() 
+        #Si todo se cumple, entra al archivo crudSistemaReserva
+        #y luego a la funcion menu admin que contiene:
+            #Administracion clientes
+            #Administracion trabajadores
+            #Administracion habitaciones
