c31adadd6899e873ca3f4901f75f6c7f38db59e7
whitebow01 <whitebownitro@gmail.com>
2024-06-22 13:56:38 -0400

    CRUD AdminCLientes basico funcional-faltan validaciones

--- crudSistemaReserva.py
+++ crudSistemaReserva.py
@@ -7,28 +7,31 @@ def default():
     
 
 
-#TRABAJADORES
-def administrarTrabajadores():
-    listaTrabajadores = registros.registroTrabajadores
+# #TRABAJADORES
+# def administrarTrabajadores():
+#     listaTrabajadores = registros.registroTrabajadores
 
 
-    def listaTrab():
-        for x in listaTrabajadores:
-            print(f" {x[1]} \n {x[2]} \n {x[3]} \n {x[4]} \n")
+#     def listaTrab():
+#         for x in listaTrabajadores:
+#             print(f" {x[1]} \n {x[2]} \n {x[3]} \n {x[4]} \n")
 
-    respuesta = "si"
-    while respuesta =="si":
-        print("\n--- Administración de Clientes ---")
-        print("Opcion 1:  Listar clientes")
-        print("Opcion 2:  Registrar cliente")
-        print("Opcion 3: Modificar cliete")
-        print("Opcion 4:  Eliminar cliente")
-                
-        #opcion = int(input("ELija una opcion \n"))
-        #menuClientes.get(opcion)()
+#     menuAdminTrabaj = {1:listaTrab,5:menuAdmin}
+    
+#     respuesta = "si"
+#     while respuesta =="si":
+#         print("\n--- Administración de Clientes ---")
+#         print("Opcion 1:  Listar clientes")
+#         print("Opcion 2:  Registrar cliente")
+#         print("Opcion 3: Modificar cliete")
+#         print("Opcion 4:  Eliminar cliente")
+#         print("Opcion 5: Volver al menu principal") 
+        
+#         opcion = int(input("ELija una opcion \n"))
+#         menuAdminTrabaj.get(opcion, default)()
             
-        respuesta = input("Administrará algo mas en trabajadores?? si/no \n")
-        respuesta = respuesta.lower()
+#         respuesta = input("Administrará algo mas en trabajadores?? si/no \n")
+#         respuesta = respuesta.lower()
 
 #CLIENTES
 def administracionClientes():
@@ -36,42 +39,46 @@ def administracionClientes():
 
     def vistaClientes():
         for x in listaClientes:
+            print("==============================")
             print(f" Rut: {x[1]} \n  Nombre: {x[2]} \n Apellido:{x[3]}")
-    
-    
-    # def modificarCliente():
-    #     bandera = False
-    #     encontrado = False
-        
-    #     rutCliente = input("Ingresa el rut del cliente")
-    #     if rutCliente in listaClientes:
-    #         print("Cliente encontrado")
-    #     nombre = input("Ingrese rut a modificar")
-
-    #     for x in listaClientes:
-    #         bandera = rutCliente in x
-    #         if (bandera):
-    #             aux = listaClientes.index(x)
-    #             encontrado = True
-    #         if (encontrado):
-    #             listaClientes[aux][2] = nombre
-    #         else:
-    #             print("El cliente no esta registrado")    
-    
+            print("==============================")
+            
+    #REGISTRAR CLIENTES DIRECTO AL MENU
+            
     def eliminarCliente():
         rutCliente = input("Ingresa el rut del cliente")
-        encontrado = False
         for x in listaClientes:
             if x[1] == rutCliente:
                 listaClientes.remove(x)
-                encontrado = True
                 print("Cliente eliminado")
                 break
             else:
                 print("Cliente no encontrado")
+    
+    def modificarCliente():
+        encontrado = False
+        
+        rutCliente = input("Ingresa el rut del cliente")
+        for cliente in listaClientes:
+            if rutCliente == cliente[1]:
+                print("Cliente encontrado")
+            nombre = input("Ingrese nombre a modificar")
+            cliente[2] = nombre
+            encontrado = True
+            print(f"Nombre cambiado a {nombre}")
+            break
+        if not encontrado:
+            print("No se encuentra el rut del cliente")
+
+    
                 
     #DICCIONARIO CON MENU - ADMINISTRACION CLIENTES       
-    menuClientes = {1: vistaClientes, 2:registros.registroCliente, 4:eliminarCliente}
+    menuClientes = {1: vistaClientes, 
+                    2:registros.registroCliente, 
+                    3:modificarCliente,
+                    4:eliminarCliente, 
+                    5:menuAdmin
+                    }
     
     respuesta = "si"
     while respuesta =="si":
@@ -80,9 +87,10 @@ def administracionClientes():
         print("Opcion 2:  Registrar cliente")
         print("Opcion 3: Modificar cliete")
         print("Opcion 4:  Eliminar cliente")
+        print("Opcion 5: Volver al menu principal")
             
         opcion = int(input("ELija una opcion \n"))
-        menuClientes.get(opcion)()
+        menuClientes.get(opcion,default)()
         
         respuesta = input("Administrará algo mas en clientes?? si/no \n")
         respuesta = respuesta.lower()
@@ -90,23 +98,23 @@ def administracionClientes():
         
 
 #HABITACIONES
-listaHabitaciones = registros.registroHabitaciones
-def administracionHabitaciones():
+# listaHabitaciones = registros.registroHabitaciones
+# def administracionHabitaciones():
     
     
-    respuesta = "si"
-    while respuesta =="si":
-        print("\n--- Administración de Clientes ---")
-        print("Opcion 1:  Listar clientes")
-        print("Opcion 2:  Registrar cliente")
-        print("Opcion 3: Modificar cliete")
-        print("Opcion 4:  Eliminar cliente")
+#     respuesta = "si"
+#     while respuesta =="si":
+#         print("\n--- Administración de Clientes ---")
+#         print("Opcion 1:  Listar clientes")
+#         print("Opcion 2:  Registrar cliente")
+#         print("Opcion 3: Modificar cliete")
+#         print("Opcion 4:  Eliminar cliente")
             
-        opcion = int(input("ELija una opcion \n"))
-        #menuClientes.get(opcion)()
+#         opcion = int(input("ELija una opcion \n"))
+#         #menuClientes.get(opcion)()
         
-        respuesta = input("Administrará algo mas en habitaciones?? si/no \n")
-        respuesta = respuesta.lower()
+#         respuesta = input("Administrará algo mas en habitaciones?? si/no \n")
+#         respuesta = respuesta.lower()
 
 #RESERVACIONES
 
@@ -127,7 +135,7 @@ def menuAdmin():
         print("3. Administrar habitaciones")
         
         opcion = int(input("ELija una opcion \n"))
-        menu.get(opcion)()
+        menu.get(opcion, default)()
 
         respuesta = input("Quiere hacer algo mas en el menu general? si/no \n")
         respuesta = respuesta.lower()
