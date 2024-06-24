22724d0bcdb7273fa3cf61e8480fe0a43f158bc6
whitebow01 <whitebownitro@gmail.com>
2024-06-23 23:05:06 -0400

    ADD se implementa validacion para salir y entrar al menu, whit try except and bucle while

--- menuGeneral.py
+++ menuGeneral.py
@@ -2,37 +2,48 @@ import CRUD.administracionTrabajadores
 import login
 
 
-menuLogin={1:login.loginTrabajador,
-        2:CRUD.administracionTrabajadores.registroTrabajador,
-        3:exit
-        }
 
 def default():
     print("Opcion fuera de rango")
     return
 
+def menuGeneralInicio():
+    menuLogin={1:login.loginTrabajador,
+            2:CRUD.administracionTrabajadores.registroTrabajador,
+            3:exit
+            }
 
-respuesta="si"
 
-while respuesta == "si":
 
-    print("╔════════════════════════════════════════════════╗")
-    print("║ Bienvenido al menú general                     ║")
-    print("║                                                ║")
-    print("║ Eliga su opción:                               ║")
-    print("║                                                ║")
-    print("║ Opción 1 > Iniciar sesión como administrador   ║")
-    print("║                                                ║")
-    print("║ Opción 2 > Registrar administrador             ║")
-    print("║                                                ║")
-    print("║ Opción 3 > Salir                               ║")
-    print("║                                                ║")
-    print("╚════════════════════════════════════════════════╝")
-    
-    
-    opcion = int(input("Elija una opcion\n"))
-    menuLogin.get(opcion,default)()                   
-    if opcion == 3:
-        break
-    respuesta = input("Desea hacer algo más?: si/no \n")
-    respuesta = respuesta.lower()
+    respuesta="si"
+
+    while respuesta == "si":
+
+        print("╔════════════════════════════════════════════════╗")
+        print("║ Bienvenido al menú general                     ║")
+        print("║                                                ║")
+        print("║ Eliga su opción:                               ║")
+        print("║                                                ║")
+        print("║ Opción 1 > Iniciar sesión como administrador   ║")
+        print("║                                                ║")
+        print("║ Opción 2 > Registrar administrador             ║")
+        print("║                                                ║")
+        print("║ Opción 3 > Salir                               ║")
+        print("║                                                ║")
+        print("╚════════════════════════════════════════════════╝")
+        
+        #validacion solo numeros
+        try:
+            opcion = int(input("Elija una opcion\n"))
+            menuLogin.get(opcion,default)()   
+        except ValueError:
+            print("Error. Por favor, ingrese solo numeros")
+        #validacion si o no
+        while respuesta not in ["si", "no"]: 
+            print("Error, debe responder si o no")    
+            respuesta = input("Desea hacer algo más?: si/no \n")
+            respuesta = respuesta.lower()
+            
+        print("Gracias por usar el programa!, Hasta pronto")
+        
+menuGeneralInicio()
